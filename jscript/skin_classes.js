323660173a14e8480ad8fda41de24b710d3e0de4
CzaplaKarolina <karolina.czapla@onet.com.pl>
2023-08-26 18:47:52 +0200

    comm

--- jscript/skin_classes.js
+++ jscript/skin_classes.js
@@ -3,7 +3,7 @@
 SKIN_CLASSES = {
   0: 'akiec, Rogowacenie słoneczne (Actinic Keratoses/Solar Keratoses) lub Nabłoniak (intraepithelial Carcinoma - Bowen’s disease)',
   1: 'bcc, Nabłoniak podstawnokomórkowy (Basal Cell Carcinoma)',
-  2: 'bkl, Łagone rogowacenie(Benign Keratosis)',
+  2: 'bkl, Łagodne rogowacenie(Benign Keratosis)',
   3: 'df, Dermatofibroma',
   4: 'mel, Czerniak (Melanoma)',
   5: 'nv, Znamię melanocytowe (Melanocytic Nevi)',
--- jscript/target_classes.js
+++ jscript/target_classes.js
@@ -18,10 +18,10 @@
 TARGET_CLASSES = {
   0: 'akiec, Rogowacenie słoneczne (Actinic Keratoses/Solar Keratoses) lub Nabłoniak (intraepithelial Carcinoma - Bowen’s disease)',
   1: 'bcc, Nabłoniak podstawnokomórkowy (Basal Cell Carcinoma)',
-  2: 'bkl, Łagone rogowacenie(Benign Keratosis)',
+  2: 'bkl, Łagodne rogowacenie(Benign Keratosis)',
   3: 'df, Dermatofibroma',
   4: 'mel, Czerniak (Melanoma)',
-  5: 'nv, Znamię melanocytowe (Melanocytic Nevi)',
+  5: 'nv, Znamię melanocytowe (Melanocytic Nevi)', 
   6: 'vasc, Naczyniowa zmiana skórna (Vascular skin lesion)'
 
 };
